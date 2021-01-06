f3cc6beb83fd6b576121077992de456e1763b422
Rogier <rogier@blijblijblij.com>
2021-01-06 16:29:13 +0100

    Add basic function to test setup / pipeline based on first experiments

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # faas-function-node-checker
+
+Add faas function to verify whether nodes are unavailable 
+and scale down deployments running on those node(s)
+
+## env
+For the github actions to work we need to add the following
+secrets to github:
+
+```
+DOCKER_USERNAME
+DOCKER_PASSWORD
+OPENFAAS_USERNAME
+OPENFAAS_PASSWORD
+OPENFAAS_GATEWAY
+```
---
+++ node-checker/handler.rb
@@ -0,0 +1,32 @@
+# check for nodes unavailable and scale deployments
+# assigned to those nodes to 0
+
+require 'kubeclient'
+require 'logging'
+
+# Main entry point to the function
+class Handler
+  def run(body, _headers)
+    status_code = 200 # Optional status code, defaults to 200
+    response_headers = { 'content-type' => 'text/plain' }
+
+    logging = Logging.logger(STDOUT)
+    logging.info('#############################')
+    logging.info 'Some logging from the installed logging gem'
+    logging.info('#############################')
+
+    auth_options = {
+      bearer_token: ENV['K3S_BEARER_TOKEN']
+    }
+
+    client = Kubeclient::Client.new(
+      'https://localhost:8443/api',
+      'v1',
+      auth_options: auth_options
+    )
+    logging.info client.to_s
+    body = "Hello world from the Ruby template #{Time.now}"
+
+    [body, response_headers, status_code]
+  end
+end
